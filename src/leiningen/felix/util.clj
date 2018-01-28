2ccc4a0e39279953ae1e3e565e0c4268823c2fd8
Duncan McGreggor <oubiwann@gmail.com>
2018-01-28 14:49:03 -0600

    Added new utility functions for shell commands.

--- src/leiningen/felix/util.clj
+++ src/leiningen/felix/util.clj
@@ -1,4 +1,9 @@
-(ns leiningen.felix.util)
+(ns leiningen.felix.util
+  (:require
+    [clojure.java.io :as io]
+    [clojure.java.shell :as shell]
+    [clojure.string :as string]
+    [leiningen.core.main :as lein]))
 
 (defn deep-merge
   "Recursively merges maps. If values are not maps, the last value wins."
@@ -14,3 +19,28 @@
 (defn subcommand
   [args]
   (keyword (first args)))
+
+(defn- get-output
+  [result out-type]
+  (let [output (out-type result)]
+    (if-not output
+      output
+      (let [output (string/trim output)]
+        (if (= "" output)
+          nil
+          output)))))
+
+(defn get-output-flag
+  [args]
+  (case (subcommand args)
+    :-v :verbose
+    :verbose :verbose
+    :quiet))
+
+(defn sh
+  [mode & args]
+  (let [result (apply shell/sh args)]
+    (case mode
+      :verbose (when-let [out (get-output result :out)] (println out))
+      :quiet (when-let [out (get-output result :out)] (lein/debug out)))
+    (when-let [err (get-output result :err)] (println err))))
