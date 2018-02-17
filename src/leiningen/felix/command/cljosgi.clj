9983bfc927750f5539cc31dc4ba05f5bae3c0be2
Duncan McGreggor <oubiwann@gmail.com>
2018-02-17 16:44:12 -0600

    Added support for uninstalling ClojureOSGi bundle.

--- src/leiningen/felix/command/cljosgi.clj
+++ src/leiningen/felix/command/cljosgi.clj
@@ -8,19 +8,6 @@
 
 (def clojure-osgi-id 'com.theoryinpractise/clojure.osgi)
 
-(defn- get-clojure-osgi-jarname
-  [version]
-  (format "%s-%s.jar"
-          (second (string/split (str clojure-osgi-id) #"/"))
-          version))
-
-(defn- get-clojure-osgi-jar
-  [proj version]
-  (->> (classpath/get-classpath proj)
-       (map #(when (string/ends-with? % (get-clojure-osgi-jarname version)) %))
-       (remove nil?)
-       first))
-
 (defn- get-clojure-osgi-version
   [proj]
   (->> proj
@@ -29,10 +16,23 @@
        (remove nil?)
        first))
 
+(defn- get-clojure-osgi-jarname
+  [proj]
+  (format "%s-%s.jar"
+          (second (string/split (str clojure-osgi-id) #"/"))
+          (get-clojure-osgi-version proj)))
+
+(defn- get-clojure-osgi-jar
+  [proj]
+  (->> (classpath/get-classpath proj)
+       (map #(when (string/ends-with? % (get-clojure-osgi-jarname proj)) %))
+       (remove nil?)
+       first))
+
 (defn install
   "Usage: lein felix clojure-osgi install JAR|[OPTIONS|SUBCOMMANDS]
 
-  Install the given OSGi bundle into Felix.
+  Install the Clojure OSGi bundle into Felix.
 
   Allowed options:
     -v - Display verbose output of install operation
@@ -45,11 +45,38 @@
   * :felix :install-dir
   * :felix :bundle-dir"
   [proj [subcommand & args]]
-  (let [version (get-clojure-osgi-version proj)
-        jar (get-clojure-osgi-jar proj version)
-        option [(second args)]]
-    (util/sh (util/get-output-flag option)
-               "cp" "-v" jar (data/bundle-dir proj))))
+  (case (util/subcommand subcommand)
+    :help (util/help #'install)
+    (let [version (get-clojure-osgi-version proj)
+          jar (get-clojure-osgi-jar proj)
+          option [(first args)]]
+      (util/sh (util/get-output-flag option)
+                 "cp" "-v" jar (data/bundle-dir proj)))))
+
+(defn uninstall
+  "Usage: lein felix clojure-osgi uninstall JAR|[OPTIONS|SUBCOMMANDS]
+
+  Uninstall the Clojure OSGi bundle from the Felix bundle directory.
+
+  Allowed options:
+    -v - Display verbose output of uninstall operation
+
+  Allowed subcommands:
+    help - Display this help message.
+
+  This command uses the following configuration options:
+
+  * :felix :install-dir
+  * :felix :bundle-dir"
+  [proj [subcommand & args]]
+  (case (util/subcommand subcommand)
+    :help (util/help #'install)
+    (let [jar (get-clojure-osgi-jarname proj)
+          option [(first args)]]
+      (util/sh (util/get-output-flag option)
+               "rm" "-v" (format "%s/%s"
+                                 (data/bundle-dir proj)
+                                 jar)))))
 
 (defn run
   "Usage: lein felix clojusc-orgi [SUBCOMMAND]
@@ -57,9 +84,11 @@
   Pergform various operations related to Clojure OSGi support.
 
   Allowed subcommands:
-    help    - Display this help message.
-    install - Install the clojure.osgi bundle."
+    help      - Display this help message.
+    install   - Install the clojure.osgi bundle.
+    uninstall - Uninstall the clojure.osgi bundle."
   [proj args]
   (case (util/subcommand args)
     :install (install proj args)
+    :uninstall (uninstall proj args)
     (util/help #'run)))
