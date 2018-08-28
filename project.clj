e748ff0c1f8688cb1e3a09a9ae80d8cce6161439
Duncan McGreggor <oubiwann@gmail.com>
2018-08-28 10:51:56 -0500

    Added aliases and profiles.

--- project.clj
+++ project.clj
@@ -10,6 +10,61 @@
   :license {
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
-    ;[org.clojure/data.zip "0.1.2"]
-  :repl-options {
-    :prompt ~get-prompt})
+  :profiles {
+    :ubercompile {
+      :aot :all}
+    :dev {
+      :dependencies [
+        [clojusc/trifl "0.3.0"]
+        [org.clojure/tools.namespace "0.2.11"]]
+      :plugins [
+        [venantius/ultra "0.5.2"]]
+      :source-paths ["dev-resources/src"]
+      :repl-options {
+        :prompt ~get-prompt}}
+    :lint {
+      :source-paths ^:replace ["src"]
+      :test-paths ^:replace []
+      :plugins [
+        [jonase/eastwood "0.2.9"]
+        [lein-ancient "0.6.15"]
+        [lein-kibit "0.1.6"]]}
+    :test {
+      :plugins [
+        [lein-ltest "0.3.0"]]
+      :test-selectors {
+        :unit #(not (or (:integration %) (:system %)))
+        :integration :integration
+        :system :system
+        :default (complement :system)}}}
+  :aliases {
+    ;; Dev Aliases
+    "repl" ["do"
+      ["clean"]
+      ["repl"]]
+    "ubercompile" ["do"
+      ["clean"]
+      ["with-profile" "+ubercompile" "compile"]]
+    "check-vers" ["with-profile" "+lint" "ancient" "check" ":all"]
+    "check-jars" ["with-profile" "+lint" "do"
+      ["deps" ":tree"]
+      ["deps" ":plugin-tree"]]
+    "check-deps" ["do"
+      ["check-jars"]
+      ["check-vers"]]
+    "kibit" ["with-profile" "+lint" "kibit"]
+    "eastwood" ["with-profile" "+lint" "eastwood" "{:namespaces [:source-paths] :continue-on-exception true}"]
+    "lint" ["do"
+      ["kibit"]
+      ; ["eastwood"]
+      ]
+    "ltest" ["with-profile" "+test" "ltest"]
+    "ltest-clean" ["do"
+      ["clean"]
+      ["ltest"]]
+    "build" ["do"
+      ["clean"]
+      ["check-vers"]
+      ["lint"]
+      ["ltest" ":all"]
+      ["uberjar"]]})
